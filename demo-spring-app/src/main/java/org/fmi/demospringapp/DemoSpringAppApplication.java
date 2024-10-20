278eead0a9de9258118ff2db723b9124c654baf9
Angel <48069264+velk20@users.noreply.github.com>
2024-10-20 11:06:38 +0300

    Spring

---
+++ demo-spring-app/src/main/java/org/fmi/demospringapp/DemoSpringAppApplication.java
@@ -0,0 +1,13 @@
+package org.fmi.demospringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringAppApplication.class, args);
+    }
+
+}
