dd09e11ce2f4570c7ad9a9e0dbe5110ad21040ed
Angel Mladenov <a.mladenov@seeburger.com>
2024-11-14 10:22:21 +0200

    p1-rent-a-car init

---
+++ p1-rent-a-car/src/main/java/com/fmi/master/p1_rent_a_car/P1RentACarApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.master.p1_rent_a_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1RentACarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1RentACarApplication.class, args);
+	}
+
+}
---
+++ p1-rent-a-car/src/test/java/com/fmi/master/p1_rent_a_car/P1RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.master.p1_rent_a_car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
