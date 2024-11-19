03a902c2e6d90eaca0811c0255127b7af97d612d
Angel Mladenov <a.mladenov@seeburger.com>
2024-11-19 14:10:24 +0200

    Add logger to flyway

--- p1-rent-a-car/src/main/java/com/fmi/master/p1_rent_a_car/config/FlywayShutdownHook.java
+++ p1-rent-a-car/src/main/java/com/fmi/master/p1_rent_a_car/config/FlywayShutdownHook.java
@@ -1,12 +1,14 @@
 package com.fmi.master.p1_rent_a_car.config;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FlywayShutdownHook implements DisposableBean {
-
+    private static final Logger logger = LoggerFactory.getLogger(FlywayShutdownHook.class);
     private final Flyway flyway;
 
     public FlywayShutdownHook(Flyway flyway) {
@@ -22,6 +24,6 @@ public class FlywayShutdownHook implements DisposableBean {
     public void destroy() {
         // Run a Flyway migration script to drop all tables
         flyway.clean(); // This will drop all tables, views, and related schema objects
-        System.out.println("Flyway clean executed: All tables dropped");
+        logger.info("Flyway clean executed: All tables dropped");
     }
 }
