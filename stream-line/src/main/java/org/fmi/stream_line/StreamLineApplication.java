b4e0c3451aaa0a4a0686018450bec4e6fcfa334f
Angel <48069264+velk20@users.noreply.github.com>
2024-12-09 17:15:15 +0200

    Stream line final proejct

---
+++ stream-line/src/main/java/org/fmi/stream_line/StreamLineApplication.java
@@ -0,0 +1,13 @@
+package org.fmi.stream_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamLineApplication.class, args);
+	}
+
+}
---
+++ stream-line/src/test/java/org/fmi/stream_line/StreamLineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fmi.stream_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
