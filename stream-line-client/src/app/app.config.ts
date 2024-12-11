0e0f56ba4fd565a1b6d599e38df463f5628e12e3
Angel Mladenov <a.mladenov@seeburger.com>
2024-12-11 16:39:01 +0200

    Init

--- stream-line-client/src/app/app.config.ts
+++ stream-line-client/src/app/app.config.ts
@@ -2,7 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideToastr} from "ngx-toastr";
+import {provideHttpClient} from "@angular/common/http";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    provideToastr()
+  ]
 };
---
+++ stream-line-client/src/app/utils/constant.ts
@@ -0,0 +1,6 @@
+export class Constant {
+  static BASE_URL = 'http://localhost:8080/api/v1';
+   static AUTH_URL = `${Constant.BASE_URL}/auth`;
+   static CHANNELS_URL = `${Constant.BASE_URL}/channels`;
+   static USERS_URL = `${Constant.BASE_URL}/users`;
+}
