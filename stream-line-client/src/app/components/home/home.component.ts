c90f77f733381b4dbae34808102aa97368dc7c05
Angel Mladenov <a.mladenov@seeburger.com>
2024-12-12 10:14:25 +0200

    Update conmponents

---
+++ stream-line-client/src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../header/header.component";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterLink
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
--- stream-line-client/src/app/components/nav/nav.component.ts
+++ stream-line-client/src/app/components/nav/nav.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
