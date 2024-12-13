57ef6fc74a16264e52785b2ca5733583dd5bb2a6
Angel Mladenov <a.mladenov@seeburger.com>
2024-12-13 14:41:16 +0200

    Add Friend message, filter frined messages

--- stream-line/src/main/java/org/fmi/streamline/repositories/MessageRepository.java
+++ stream-line/src/main/java/org/fmi/streamline/repositories/MessageRepository.java
@@ -3,5 +3,9 @@ package org.fmi.streamline.repositories;
 import org.fmi.streamline.entities.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<MessageEntity, String> {
+    List<MessageEntity> findAllByChannelIsNullAndReceiverIdAndAuthorId(String receiverId, String authorId);
+
 }
--- stream-line/src/main/java/org/fmi/streamline/services/MessageService.java
+++ stream-line/src/main/java/org/fmi/streamline/services/MessageService.java
@@ -1,8 +1,11 @@
 package org.fmi.streamline.services;
 
+import org.fmi.streamline.dtos.message.FriendMessageDTO;
 import org.fmi.streamline.entities.MessageEntity;
 import org.fmi.streamline.repositories.MessageRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
@@ -14,5 +17,9 @@ public class MessageService {
 
     public MessageEntity save(MessageEntity messageEntity) {
         return this.messageRepository.save(messageEntity);
+    }
+
+    public List<MessageEntity> getAllMessagesForFriend(FriendMessageDTO dto) {
+        return this.messageRepository.findAllByChannelIsNullAndReceiverIdAndAuthorId(dto.getFriendId(), dto.getUserId());
     }
 }
