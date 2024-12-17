b763602884d7b13cc4957e493c84960fe50ab2c4
Angel Mladenov <a.mladenov@seeburger.com>
2024-12-17 14:13:35 +0200

    Add styles

--- stream-line-client/src/app/components/home/home.component.ts
+++ stream-line-client/src/app/components/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from "../header/header.component";
 import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent,
     RouterLink
   ],
   templateUrl: './home.component.html',
