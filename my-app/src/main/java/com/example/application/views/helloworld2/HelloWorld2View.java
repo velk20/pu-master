8680e7f6c3bb6093476a2564142bbf9c0bde4988
Angel Mladenov <a.mladenov@seeburger.com>
2025-01-25 09:37:32 +0200

    initial vaadin project

---
+++ my-app/src/main/java/com/example/application/views/helloworld2/HelloWorld2View.java
@@ -0,0 +1,37 @@
+package com.example.application.views.helloworld2;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("Hello World2")
+@Route("hello-world")
+@Menu(order = 4, icon = LineAwesomeIconUrl.GLOBE_SOLID)
+@AnonymousAllowed
+public class HelloWorld2View extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloWorld2View() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
