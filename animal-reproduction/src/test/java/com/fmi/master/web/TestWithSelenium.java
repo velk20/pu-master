b951cbffe7ee7b9d81fe4da54811b41c98373b53
Angel Mladenov <a.mladenov@seeburger.com>
2025-04-05 11:22:37 +0300

    Selenium tests

--- animal-reproduction/src/test/java/com/fmi/master/web/TestWithSelenium.java
+++ animal-reproduction/src/test/java/com/fmi/master/web/TestWithSelenium.java
@@ -8,17 +8,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestWithSelenium {
+    private static final String SEARCH_BUTTON_XPATH = "//*[@id=\"search-form\"]/fieldset/button";
+    private static final String SEARCH_INPUT_ID = "searchInput";
     private WebDriver driver;
 
     @Before
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
         driver.manage().deleteAllCookies();
     }
 
@@ -29,12 +33,29 @@ public class TestWithSelenium {
     }
 
     @Test
-    public void testHomePage() {
+    public void testHomePage() throws InterruptedException {
         driver.get("https://www.wikipedia.org/");
         String title = driver.getTitle();
         assertEquals("Wikipedia", title);
 
-        List<WebElement> searchInput = driver.findElements(By.id("searchInput"));
+        List<WebElement> searchInput = driver.findElements(By.id(SEARCH_INPUT_ID));
         assertEquals(1, searchInput.size());
+
+        searchInput.get(0).sendKeys("Bulgaria");
+
+        List<WebElement> searchButton = driver.findElements(By.xpath(SEARCH_BUTTON_XPATH));
+        assertEquals(1, searchButton.size());
+
+        searchButton.get(0).click();
+        Thread.sleep(500);
+
+        title = driver.getTitle();
+        assertEquals("Bulgaria - Wikipedia", title);
+
+        List<WebElement> titleElement = driver.findElements(By.cssSelector("#firstHeading > span"));
+        assertEquals(1, titleElement.size());
+
+        title = titleElement.get(0).getText();
+        assertEquals("Bulgaria", title);
     }
 }
